59d5064e5973385a2f64187f22f78fa52e478a8f
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-31 16:42:00 +0300

    Updates

---
+++ precache-manifest.c71bc5d1885e04727e916bbb503398b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32cf3f9e742931d6e1e3f15a122f857",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "447235a2b751986d404f",
+    "url": "/react_tabs-with-router/static/css/main.27d0b80d.chunk.css"
+  },
+  {
+    "revision": "c3dafb983d9e67153e0c",
+    "url": "/react_tabs-with-router/static/js/2.fb26fdca.chunk.js"
+  },
+  {
+    "revision": "447235a2b751986d404f",
+    "url": "/react_tabs-with-router/static/js/main.8cc7ac39.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
